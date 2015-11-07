812b9b56b81ad664408311c2647556f8dee0a463
Rob Wilson <roobert@gmail.com>
2015-11-07 19:48:39 +0000

    add health checks

--- app.rb
+++ app.rb
@@ -3,8 +3,18 @@
 require 'webrick'
 require 'socket'
 
-port = ARGV[0] ? ARGV[0] : 4567
+port    = ARGV[0] ? ARGV[0] : 4567
+healthy = ARGV[1] ? ARGV[1] : 200
 
-server = WEBrick::HTTPServer.new(Port: port)
-server.mount_proc '/' do |_, response| response.body Socket.gethostname; end
+server  = WEBrick::HTTPServer.new(Port: port)
+
+server.mount_proc '/' do |_, response|
+  response.body Socket.gethostname
+end
+
+server.mount_proc '/health' do |_, response|
+  response.body 'OK'
+  response.status healthy
+end
+
 server.start
